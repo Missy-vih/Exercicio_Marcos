1c60c277b647d1addc71f972d44d9aa7c4e2cafd
vivian <vgrs530@gmail.com>
2024-08-01 16:35:09 -0300

    primeira versao

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace teste1.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
